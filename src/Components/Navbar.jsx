fda8e5de84fb3fef9f0327270bd2b270c25aec4c
omlan99 <omlan10616@gmail.com>
2025-02-06 17:40:08 +0600

    added navbar and footer

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import logo from "../assets/amlan-dey-high-resolution-logo-grayscale.png"
+const Navbar = () => {
+    const links = <>
+     <li>
+                <a>About</a>
+              </li>
+              <li>
+                <a>Project</a>
+                
+              </li>
+              <li>
+                <a>Contact</a>
+              </li>
+    </>
+  return (
+    <div>
+      <div className="navbar bg-base-100 fixed pr-12">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+             {links}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">Amlan Dey</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {links}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Resume</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/MainPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+
+const MainPage = () => {
+    return (
+        <div className='container mx-auto'>
+            <Navbar></Navbar>
+            <div className='min-h-screen'>
+
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default MainPage;
