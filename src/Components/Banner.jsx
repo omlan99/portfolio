41b66904470e086853f2e65c44409a1b62ae4b28
omlan99 <omlan10616@gmail.com>
2025-04-26 18:35:27 +0600

    added project array

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -7,15 +7,17 @@ import fb from "../assets/icons8-facebook-logo-50.png";
 const Banner = () => {
   return (
     <div className="grid lg:grid-cols-2 mb-[60px] py-[150px] gap-5 " id="home">
-      <div className="pl-20 flex flex-col justify-center">
+      <div className="pl-8 lg:pl-20 flex flex-col justify-center">
         <div className="flex items-center pb-5">
           <div>
             <p className="font-semibold text-2xl">Hello, I am</p>
-            <h2 className="font-bold text-6xl py-4">Amlan Dey</h2>
-            <p className="font-semibold text-3xl">A Web developer</p>
+         <div className="my-5">
+         <h2 className="font-bold text-6xl py-4">Amlan Dey</h2>
+         <p className="font-semibold text-3xl">a Web developer</p>
+         </div>
           </div>
         </div>
-        <div>
+        <div className="mb-7">
           <p className="text-xl pb-3">Let's get connected </p>
           <div className="flex gap-5">
           <a href="https://www.linkedin.com/in/omlan99" target="_blank">
@@ -32,7 +34,9 @@ const Banner = () => {
           </a>
           </div>
         </div>
+        <div >
         <button className="btn btn-neutral mt-6 btn-wide">Resume</button>
+        </div>
       </div>
       <div className="flex justify-center items-center">
         <img
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,28 +1,70 @@
-import React, { useEffect, useState } from "react"; 
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Project from "./Project";
 
 const Projects = () => {
-  const [projects, setProjects] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [selectedProject, setSelectedProject] = useState(null); 
+  // const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
+  const projects = [
+    {
+      id: "1",
+      project_name: "Lingo Bingo",
+      project_description:
+        "Lingo Bingo is a fun and interactive language learning application designed to help users expand their vocabulary and improve their communication skills in multiple languages. ",
+      image:
+        "https://i.ibb.co.com/pvccbqbq/Fire-Shot-Capture-042-Lingo-Bingo-lingo-bingo-auth-web-app.png",
+      techstack: ["React", "Axios", "Firebase", "Tailwind CSS"],
+      features: [
+        "Interactive Lessons",
+        "Responsive Design",
+        "Audio Pronunciation",
+        "Tutorial Videos",
+      ],
+      link: "https://lingo-bingo-auth.web.app/",
+      repo: "https://github.com/omlan99/Lingo-Bingo",
+    },
+    {
+      id: "2",
+      project_name: "Visa Navigator",
+      project_description:
+        "Visa Navigator is a user-friendly portal that simplifies the process of checking visa requirements, applying for visas online, and tracking visa applications.",
+      image:
+        "https://i.ibb.co.com/3yRckZ3M/Fire-Shot-Capture-039-Vite-React-visa-navigator-d47f9-web-app.png",
+      techstack: ["React", "Node.js", "Express", "MongoDB", "Tailwind CSS"],
+      features: [
+        "Visa Requirement Lookup",
+        "Visa Application Management",
+        "Apply for visa",
+        "Responsive and mobile-friendly UI",
+      ],
+      link: "https://visa-navigator-d47f9.web.app/",
+      repo: "https://github.com/omlan99/Visa_Navigator_Client",
+    },
+    {
+      id: "3",
+      project_name: "Gadget Heaven",
+      project_description:
+        "GadgetHeaven is a responsive e-commerce platform for gadget enthusiasts. Built using React, the platform provides a seamless user experience for browsing, filtering, and purchasing gadgets. ",
+      image:
+        "https://i.ibb.co.com/4Z5DGbtf/Fire-Shot-Capture-040-Home-teal-cocada-23a7aa-netlify-app.png",
+      techstack: ["React", "Tailwind CSS"],
+      features: [
+        "User can add product to wishlist",
+        "User can add product to cart",
+        "User can see list of cart products",
+        "User can see list of wishlited products",
+      ],
+      link: "https://teal-cocada-23a7aa.netlify.app/",
+      repo: "https://github.com/omlan99/Gadget-store",
+    },
+  ];
 
-  useEffect(() => {
-    axios
-      .get("https://portfolio-server-smoky-eight.vercel.app/projects")
-      .then((res) => {
-        setProjects(res.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching projects:", error);
-        setLoading(false);
-      });
-  }, []);
+ 
 
   const handleProjectClick = (project) => {
     setSelectedProject(project);
-    document.getElementById("my_modal_3").showModal(); 
+    document.getElementById("my_modal_3").showModal();
   };
 
   return (
@@ -33,18 +75,26 @@ const Projects = () => {
       <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-5 p-5">
         {loading ? (
           <div className="w-full flex justify-center">
-            <span className="loading loading-dots loading-lg"></span>
+            {/* <span className="loading loading-dots loading-lg"></span> */}
+            {console.log(projects)}
           </div>
         ) : projects.length > 0 ? (
-          projects.map((project, index) => (
-            <Project key={project.id || index} project={project} onClick={handleProjectClick} />
-          ))
+            projects.map((project, index) => (
+              
+            <Project
+              key={project.id || index}
+              project={project}
+              onClick={handleProjectClick}
+              />
+            ))
+            
         ) : (
-          <p className="text-center text-gray-500 col-span-3">No projects found.</p>
+          <p className="text-center text-gray-500 col-span-3">
+            No projects found.
+          </p>
         )}
       </div>
 
- 
       <dialog id="my_modal_3" className="modal">
         <div className="modal-box">
           <form method="dialog">
@@ -54,9 +104,9 @@ const Projects = () => {
           </form>
           {selectedProject && (
             <>
-             
               <p className="py-4">
-                <span className="font-bold text-lg" >Project Description: </span> {selectedProject.project_description}
+                <span className="font-bold text-lg">Project Description: </span>{" "}
+                {selectedProject.project_description}
               </p>
               <p className="font-bold text-lg">Tech Stack:</p>
               <div className="flex flex-wrap gap-2">
@@ -67,12 +117,41 @@ const Projects = () => {
                 ))}
               </div>
               <div className="py-4 pl-4">
-              <p className="font-bold text-lg ">Features</p>
-              <ul>
-                {selectedProject.features.map((feature, index) =><li className="list-disc" key={index}>{feature}</li>)}
-              </ul>
+                <p className="font-bold text-lg ">Features</p>
+                <ul>
+                  {selectedProject.features.map((feature, index) => (
+                    <li className="list-disc" key={index}>
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
               </div>
-
+              <div className="flex gap-3">
+                <a
+                  href={selectedProject.link}
+                  target="_blank"
+                  className="btn btn-neutral"
+                >
+                  Live Link
+                </a>
+                <a
+                  href={selectedProject.repo}
+                  target="_blank"
+                  className="btn btn-neutral"
+                >
+                  Repo Link
+                </a>
+              </div>
+              <div className="py-4">
+                <p className="font-bold text-lg">Challenges </p>
+                <li>Learning CRUD oprerations</li>
+                <li>Finishing the project on time</li>
+              </div>
+              <div className="py-4">
+                <p className="font-bold text-lg">Future Plan </p>
+                <li>To improve the ux</li>
+                <li>To add more features</li>
+              </div>
             </>
           )}
         </div>
